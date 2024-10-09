2eaa52b598cd698641a5dd47f12403fef44413e4
TheAbhiPatel <theabhipatel.co@gmail.com>
2024-10-09 21:12:15 +0530

    feat added poppins font in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,14 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Poppins } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/ThemeProvider";
 import AppLayout from "./AppLayout";
 
-const inter = Inter({ subsets: ["latin"] });
+const poppins = Poppins({
+  weight: "400",
+  subsets: ["latin"],
+  variable: "--font-poppins",
+});
 
 export const metadata: Metadata = {
   title: "TheAbhiPatel | Portfolio",
@@ -21,7 +25,7 @@ export default function RootLayout({
       lang="en"
       suppressHydrationWarning
     >
-      <body className={inter.className}>
+      <body className={poppins.className}>
         <ThemeProvider
           attribute="class"
           defaultTheme="dark"
