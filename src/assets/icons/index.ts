cd68ed3d097a64fadc4afa7db12efcd8dcd8835d
TheAbhiPatel <theabhipatel.co@gmail.com>
2024-10-13 15:43:53 +0530

    chore import all the png icons and export them all with index.ts file

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,151 @@
+import androidIcon from "./android.png";
+import apacheIcon from "./apache.png";
+import kafkaIcon from "./apache_kafka.png";
+import appleIcon from "./apple.png";
+import safariIcon from "./apple_safari.png";
+import awsIcon from "./aws.png";
+import axiosIcon from "./axios.png";
+import azureIcon from "./azure.png";
+import babelIcon from "./babel.png";
+import bitbucketIcon from "./bitbucket.png";
+import bootstrapIcon from "./bootstrap.png";
+import bunIcon from "./bun.png";
+import chromeIcon from "./chrome.png";
+import cloudflareIcon from "./cloudflare.png";
+import css3Icon from "./css3.png";
+import digitalOceanIcon from "./digital_ocean.png";
+import dockerIcon from "./docker.png";
+import eslintIcon from "./eslint.png";
+import expressIcon from "./express.png";
+import figmaIcon from "./figma.png";
+import firebaseIcon from "./firebase.png";
+import firefoxIcon from "./firefox.png";
+import gitIcon from "./git.png";
+import githubIcon from "./github.png";
+import githubActionsIcon from "./github_actions.png";
+import githubCodespacesIcon from "./github_codespaces.png";
+import gitlabIcon from "./gitlab.png";
+import googleIcon from "./google.png";
+import googleCloudIcon from "./google_cloud.png";
+import graphqlIcon from "./graphql.png";
+import html5Icon from "./html5.png";
+import insomniaIcon from "./insomnia.png";
+import javascriptIcon from "./javascript.png";
+import jenkinsIcon from "./jenkins.png";
+import jestIcon from "./jest.png";
+import jiraIcon from "./jira.png";
+import jsonIcon from "./json.png";
+import kubernetesIcon from "./kubernetes.png";
+import linkedinIcon from "./linkedin.png";
+import linuxIcon from "./linux.png";
+import markdownIcon from "./markdown.png";
+import materialUiIcon from "./material_ui.png";
+import mochaIcon from "./mocha.png";
+import mongodbIcon from "./mongodb.png";
+import mongooseIcon from "./mongoose.js.png";
+import mysqlIcon from "./mysql.png";
+import nestjsIcon from "./nestjs.png";
+import nextjsIcon from "./nextjs.png";
+import nginxIcon from "./nginx.png";
+import nodejsIcon from "./nodejs.png";
+import nodemonIcon from "./nodemon.png";
+import npmIcon from "./npm.png";
+import postgresqlIcon from "./postgresql.png";
+import postmanIcon from "./postman.png";
+import reactIcon from "./react.png";
+import redisIcon from "./redis.png";
+import reduxIcon from "./redux.png";
+import sassIcon from "./sass.png";
+import slackIcon from "./slack.png";
+import socketioIcon from "./socketio.png";
+import stackCverflowIcon from "./stack_overflow.png";
+import swaggerIcon from "./swagger.png";
+import tailwindCSSIcon from "./tailwind_css.png";
+import terraformIcon from "./terraform.png";
+import threejsIcon from "./threejs.png";
+import trelloIcon from "./trello.png";
+import typescriptIcon from "./typescript.png";
+import ubuntuIcon from "./ubuntu.png";
+import vercelIcon from "./vercel.png";
+import vitejsIcon from "./vitejs.png";
+import vsCodeIcon from "./vs_code.png";
+import webpackIcon from "./webpack.png";
+import windows11Icon from "./windows11.png";
+import yarnIcon from "./yarn.png";
+
+export {
+  androidIcon,
+  apacheIcon,
+  kafkaIcon,
+  appleIcon,
+  safariIcon,
+  awsIcon,
+  axiosIcon,
+  azureIcon,
+  babelIcon,
+  bitbucketIcon,
+  bootstrapIcon,
+  bunIcon,
+  chromeIcon,
+  cloudflareIcon,
+  css3Icon,
+  digitalOceanIcon,
+  dockerIcon,
+  eslintIcon,
+  expressIcon,
+  figmaIcon,
+  firebaseIcon,
+  firefoxIcon,
+  gitIcon,
+  githubIcon,
+  githubActionsIcon,
+  githubCodespacesIcon,
+  gitlabIcon,
+  googleIcon,
+  googleCloudIcon,
+  graphqlIcon,
+  html5Icon,
+  insomniaIcon,
+  javascriptIcon,
+  jenkinsIcon,
+  jestIcon,
+  jiraIcon,
+  jsonIcon,
+  kubernetesIcon,
+  linkedinIcon,
+  linuxIcon,
+  markdownIcon,
+  materialUiIcon,
+  mochaIcon,
+  mongodbIcon,
+  mongooseIcon,
+  mysqlIcon,
+  nestjsIcon,
+  nextjsIcon,
+  nginxIcon,
+  nodejsIcon,
+  nodemonIcon,
+  npmIcon,
+  postgresqlIcon,
+  postmanIcon,
+  reactIcon,
+  redisIcon,
+  reduxIcon,
+  sassIcon,
+  slackIcon,
+  socketioIcon,
+  stackCverflowIcon,
+  swaggerIcon,
+  tailwindCSSIcon,
+  terraformIcon,
+  threejsIcon,
+  trelloIcon,
+  typescriptIcon,
+  ubuntuIcon,
+  vercelIcon,
+  vitejsIcon,
+  vsCodeIcon,
+  webpackIcon,
+  windows11Icon,
+  yarnIcon,
+};
