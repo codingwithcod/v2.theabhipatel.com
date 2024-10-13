c35c12e1c6172d57cd5bcea1618b12a0bada65ec
TheAbhiPatel <theabhipatel.co@gmail.com>
2024-10-13 15:44:52 +0530

    update constant skills file and used all png image icons instead of react-icons

---
+++ src/constants/skills.ts
@@ -0,0 +1,214 @@
+import {
+  bootstrapIcon,
+  css3Icon,
+  expressIcon,
+  html5Icon,
+  javascriptIcon,
+  materialUiIcon,
+  mongodbIcon,
+  nestjsIcon,
+  nextjsIcon,
+  nodejsIcon,
+  reactIcon,
+  tailwindCSSIcon,
+  typescriptIcon,
+  mysqlIcon,
+  postgresqlIcon,
+  dockerIcon,
+  nginxIcon,
+  apacheIcon,
+  awsIcon,
+  linuxIcon,
+  ubuntuIcon,
+  windows11Icon,
+  npmIcon,
+  yarnIcon,
+  gitIcon,
+  githubIcon,
+  gitlabIcon,
+  axiosIcon,
+  reduxIcon,
+  vsCodeIcon,
+  mongooseIcon,
+  figmaIcon,
+  firebaseIcon,
+  postmanIcon,
+  webpackIcon,
+  jestIcon,
+  mochaIcon,
+  markdownIcon,
+  appleIcon,
+} from "@/assets/icons";
+
+// ---->Tech icons website => https://techicons.dev/
+
+export const mySkills = [
+  {
+    name: "React JS",
+    icon: reactIcon,
+  },
+  {
+    name: "Next JS",
+    icon: nextjsIcon,
+  },
+  {
+    name: "Node JS",
+    icon: nodejsIcon,
+  },
+  {
+    name: "React Native",
+    icon: reactIcon,
+  },
+  {
+    name: "Nest JS",
+    icon: nestjsIcon,
+  },
+  {
+    name: "JavaScript",
+    icon: javascriptIcon,
+  },
+  {
+    name: "TypeScript",
+    icon: typescriptIcon,
+  },
+  {
+    name: "Mongo DB",
+    icon: mongodbIcon,
+  },
+  {
+    name: "Express JS",
+    icon: expressIcon,
+  },
+  {
+    name: "Tailwind CSS",
+    icon: tailwindCSSIcon,
+  },
+  {
+    name: "Bootstrap",
+    icon: bootstrapIcon,
+  },
+  {
+    name: "Mui",
+    icon: materialUiIcon,
+  },
+  {
+    name: "HTML5",
+    icon: html5Icon,
+  },
+  {
+    name: "CSS3",
+    icon: css3Icon,
+  },
+  {
+    name: "SQL",
+    icon: mysqlIcon,
+  },
+  {
+    name: "My SQL",
+    icon: mysqlIcon,
+  },
+  {
+    name: "PostgreSQL",
+    icon: postgresqlIcon,
+  },
+  {
+    name: "Docker",
+    icon: dockerIcon,
+  },
+  {
+    name: "Nginx",
+    icon: nginxIcon,
+  },
+  {
+    name: "Apache",
+    icon: apacheIcon,
+  },
+  {
+    name: "AWS",
+    icon: awsIcon,
+  },
+  {
+    name: "Linux",
+    icon: linuxIcon,
+  },
+  {
+    name: "MacOS",
+    icon: appleIcon,
+  },
+  {
+    name: "Ubuntu",
+    icon: ubuntuIcon,
+  },
+  {
+    name: "Windows",
+    icon: windows11Icon,
+  },
+  {
+    name: "NPM",
+    icon: npmIcon,
+  },
+  {
+    name: "Yarn",
+    icon: yarnIcon,
+  },
+  {
+    name: "PNPM",
+    icon: npmIcon,
+  },
+  {
+    name: "GIT",
+    icon: gitIcon,
+  },
+  {
+    name: "Github",
+    icon: githubIcon,
+  },
+  {
+    name: "GitLab",
+    icon: gitlabIcon,
+  },
+  {
+    name: "Axios",
+    icon: axiosIcon,
+  },
+  {
+    name: "Redux/Tookit",
+    icon: reduxIcon,
+  },
+  {
+    name: "Vs code",
+    icon: vsCodeIcon,
+  },
+  {
+    name: "Mongoose",
+    icon: mongooseIcon,
+  },
+  {
+    name: "Figma",
+    icon: figmaIcon,
+  },
+  {
+    name: "Firebase",
+    icon: firebaseIcon,
+  },
+  {
+    name: "Postman",
+    icon: postmanIcon,
+  },
+  {
+    name: "Webpack",
+    icon: webpackIcon,
+  },
+  {
+    name: "Jest",
+    icon: jestIcon,
+  },
+  {
+    name: "Mocha",
+    icon: mochaIcon,
+  },
+  {
+    name: "Markdown",
+    icon: markdownIcon,
+  },
+];
